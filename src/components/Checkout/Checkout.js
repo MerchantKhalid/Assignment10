416dfcf7f68533538cb777f2ff6b61d372e2515f
MerchantKhalid88 <mohammadkhr88@gmail;com>
2022-04-18 00:30:35 +0200

    cart added

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const Checkout = () => {
   return (
     <div>
-      <h2>checkout</h2>
+      <div className="container mx-auto w-50 mt-5 text-left">
+        <h2 className="fw-bold text-dark">Cart</h2>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="text" placeholder="Your Name" required />
+          </Form.Group>
+          <Form.Group readonly className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Enter email" required />
+          </Form.Group>
+          <Form.Group readonly className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="text" placeholder="Address" required />
+          </Form.Group>
+          <Form.Group readonly className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="text" placeholder="Phone" required />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Order
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
